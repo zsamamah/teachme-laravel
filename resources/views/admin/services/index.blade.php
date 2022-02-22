73f33cfa2bdec937c38adcdd42331f3a38378723
“Azmi <Azmitammam@gmail.com>
2022-02-22 11:09:47 +0200

    Edit admin view

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -2,43 +2,43 @@
 
 @section('content')
 @if ($message = session('success'))
-    <div class="alert alert-success">
-        {{ $message }}
-    </div>
+<div class="alert alert-success">
+    {{ $message }}
+</div>
 @endif
-    <div class="card">
-        <div class="card-header">
-            <h1>Services Page</h1>
-            <a href="/services-dashboard/create" class="btn btn-primary">Add New Test!</a>
-            <hr>
-        </div>
-        <div class="card-body">
-            <table class="table table-responsive table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Test Name</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($services as $item)
-                    <tr class="border">
-                        <td>{{ $item->id }}</td>
-                        <td >{{ $item->service }}</td>
-                        <td>
-                            <a href="{{ route('editService',$item->id) }}" class="btn btn-primary">Edit</a>
-                            <form method="POST" action="{{route('delete-service',$item->id)}}">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-
-            </table>
-        </div>
+<div class="card">
+    <div class="card-header">
+        <h1>Services Page</h1>
+        <a href="/services-dashboard/create" class="btn btn-primary">Add New Test!</a>
+        <hr>
     </div>
+    <div class="card-body">
+        <table class="table table-responsive table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Test Name</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($services as $item)
+                <tr class="border">
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->service }}</td>
+                    <td>
+                        <a href="{{ route('editService',$item->id) }}" class="btn btn-primary">Edit</a>
+                        <form method="POST" style="display: inline;" action="{{route('delete-service',$item->id)}}">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" style="display: inline;" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+
+        </table>
+    </div>
+</div>
 @endsection
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -2,49 +2,49 @@
 
 @section('content')
 @if ($message = session('success'))
-    <div class="alert alert-success">
-        {{ $message }}
-    </div>
+<div class="alert alert-success">
+    {{ $message }}
+</div>
 @endif
-    <div class="card">
-        <div class="card-header">
-            <h1>Users Page</h1>
-            <a href="{{ url('add-user') }}" class="btn btn-primary">Add User!</a>
-            <hr>
-        </div>
-        <div class="card-body">
-            <table class="table table-responsive table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Email</th>
-                        <th>Name</th>
-                        <th>Role</th>
-                        <th>Action</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($user as $item)
-                    <tr class="border">
-                        <td>{{ $item->id }}</td>
-                        <td>{{ $item->email }}</td>
-                        <td >{{ $item->name }}</td>
-                        <td>{{ $item->email == 'admin@admin.com' ? 'Admin' : 'User'}}</td>
-                        <td>
-                            <a href="{{ route('users-edit',$item->id) }}" class="btn btn-primary">Edit</a>
-                            {{-- replace anchor with form which method is delete --}}
-                            <form method="POST" action="{{ route('delete-user',$item->id) }}">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-
-            </table>
-        </div>
+<div class="card">
+    <div class="card-header">
+        <h1>Users Page</h1>
+        <a href="{{ url('add-user') }}" class="btn btn-primary">Add User!</a>
+        <hr>
     </div>
+    <div class="card-body">
+        <table class="table table-responsive table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Action</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($user as $item)
+                <tr class="border">
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->email }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->email == 'admin@admin.com' ? 'Admin' : 'User'}}</td>
+                    <td>
+                        <a href="{{ route('users-edit',$item->id) }}" class="btn btn-primary">Edit</a>
+                        {{-- replace anchor with form which method is delete --}}
+                        <form method="POST" style="display: inline;" action="{{ route('delete-user',$item->id) }}">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" style="display: inline;" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+
+        </table>
+    </div>
+</div>
 @endsection
